79f127fa6ab5f5c4561b3ec566e6c73c8884ff6d
Caio Marinho <caio_mcoelho@hotmail.com>
2020-06-24 13:25:42 -0300

    Projeto - Palavra palíndromo v02

---
+++ src/Teste01_v2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Teste01_v2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("-------------- DESAFIO 01 --------------");
+		System.out.println();
+		System.out.println("--------- PALAVRAS PALÍNDROMOS ---------");
+		System.out.println();
+		
+		//Declaração da String utilizada
+		String p1;
+		
+		//Scanner para ler a entrada do usuário
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite a palavra desejada: ");
+		
+		//Foram utilizadar as funções TRIM para remover espaços antes, TOUPPERCASE para colocar tudo em maiúsculo e REPLACEALL para remover os espaços entre as strings
+		p1 = ler.nextLine().trim().toUpperCase().replaceAll(" ", "");
+		
+		//Variaveis criadas para pegar o tamanho da string
+		Integer l1 = p1.length();
+		Integer cont = l1;
+		
+		//Variável usada para test dentro do laço
+		int test = 0;
+		
+		//Irá percorrer a string do inicio até a metade
+		for(int x = 0; x < l1/2; x++) {
+			char var1 = p1.charAt(x);
+			//Irá pegar a stringo ao inverso (de tras pra frente)
+			char var2 = p1.charAt(cont-1);
+			
+			//Compara se as duas string são iguais
+			if(var1 != var2) {
+				System.out.println("A palavra "+p1+" não é palíndromo");
+				//Somar 1 caso tenha uma letra diferente
+				test ++;
+				break;
+			}
+			//Decremento para percorrer a string ao contrário
+			cont --;
+		}
+		
+		//Verifica se o test == 0, se for quer dizer que ele não teve nenhuma string diferente
+		if(test == 0) {
+			System.out.println("A palavra "+p1+" é palíndromo");
+		}
+	}
+}
